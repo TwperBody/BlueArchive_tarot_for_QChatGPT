4a7d03632b2dfe910958d3ba255bab8b9bbb942d
TwperBody <mcjiekejiemi@163.com>
2024-10-06 16:04:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ class TarotCardPlugin(BasePlugin):
 
     @handler(GroupNormalMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
-        self.handle_tarot_card(ctx)
+        self.handle_tarot_card_g(ctx)
 
     def handle_tarot_card(self, ctx: EventContext):
         msg = ctx.event.text_message
-        if  "塔罗牌" in msg:
+        if  msg == "塔罗牌":
             random_number = random.randint(0, 21)
             image_path = self.get_image_path(random_number)
             message_chain = MessageChain([
@@ -32,6 +32,21 @@ class TarotCardPlugin(BasePlugin):
                 Image(path=str(image_path))
             ])
             ctx.add_return("reply", message_chain)
+            ctx.prevent_default()
+            
+
+        def handle_tarot_card_g(self, ctx: EventContext):
+        msg = ctx.event.text_message
+        if  msg == " 塔罗牌":
+            random_number = random.randint(0, 21)
+            image_path = self.get_image_path(random_number)
+            message_chain = MessageChain([
+                Plain("老师，这是你今天的塔罗牌"),
+                Image(path=str(image_path))
+            ])
+            ctx.add_return("reply", message_chain)
+            ctx.prevent_default()
+            
 
     def get_image_path(self, random_number):
         return Path(f"plugins/QChatGPT_BlueArchive_tarot/image/{random_number}.png")
