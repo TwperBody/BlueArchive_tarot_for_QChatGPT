8083020ab33ed8dfb6f0bc8fae54c4d963837370
TwperBody <mcjiekejiemi@163.com>
2024-10-06 16:07:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,24 +20,11 @@ class TarotCardPlugin(BasePlugin):
 
     @handler(GroupNormalMessageReceived)
     async def group_normal_message_received(self, ctx: EventContext):
-        self.handle_tarot_card_g(ctx)
+        self.handle_tarot_card(ctx)
 
     def handle_tarot_card(self, ctx: EventContext):
         msg = ctx.event.text_message
         if  msg == "塔罗牌":
-            random_number = random.randint(0, 21)
-            image_path = self.get_image_path(random_number)
-            message_chain = MessageChain([
-                Plain("老师，这是你今天的塔罗牌"),
-                Image(path=str(image_path))
-            ])
-            ctx.add_return("reply", message_chain)
-            ctx.prevent_default()
-            
-
-        def handle_tarot_card_g(self, ctx: EventContext):
-        msg = ctx.event.text_message
-        if  msg == " 塔罗牌":
             random_number = random.randint(0, 21)
             image_path = self.get_image_path(random_number)
             message_chain = MessageChain([
